a88648ded52401ef52b599f9c02ec95e3dd44336
Ameen_Works <ameen.desk@gmail.com>
2023-12-23 16:10:01 +0530

    updated

--- Routes/TaskRoutes.js
+++ Routes/TaskRoutes.js
@@ -7,7 +7,7 @@ const authenticate = require("../Middlewares/authMiddleware");
 const getAllTasksForUser = require("../ControllerActions/TaskController/getTask");
 const deleteTask = require("../ControllerActions/TaskController/deleteTask");
 const editTask = require("../ControllerActions/TaskController/editTask");
-const updateTaskCompletionStatus = require("../ControllerActions/TaskController/updatecompletion");
+const updateTaskCompletionStatus = require("../ControllerActions/TaskController/updateCompletion.js");
 
 taskRouter.post("/add-task", authenticate, addTaskAction);
 taskRouter.get("/all-tasks", authenticate, getAllTasksForUser);
